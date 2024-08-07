a5edb29a2b9e885bde0c0fe1081b0b696fa4b807
Nekopunch <Nekopunch2482@yandex.ru>
2024-08-07 18:57:25 +0300

    feat: tests are passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const config = bracketsConfig.map(([o, c]) => o + c);
+
+  let last_try = str.length + 1;
+
+  while (str.length < last_try) {
+    last_try = str.length;
+    config.forEach((e) => {
+      str = str.replace(e, "");
+    });
+  }
+
+  return !str.length;
+};
